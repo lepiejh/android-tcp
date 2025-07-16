964c872859e42a62648d28085cacdca6838dfcdd
lixiong <qqlixiongjob@163.com>
2025-07-16 10:19:37 +0800

    ...

--- tcp/src/main/java/com/ved/tcp/Request.kt
+++ tcp/src/main/java/com/ved/tcp/Request.kt
@@ -1,7 +1,7 @@
 package com.ved.tcp
 
 object Request {
-    fun req(z: Boolean = false,h: Boolean = false,w:Boolean = false,r:Boolean = false,m:Boolean = false,d:Boolean = false,cc:Boolean = false,st:Boolean = false,t:Int = 5,c:String? = null,u:String? = null,p:Int,dm:Long = 100,sm:Long = 100,s: List<String>?,callBack: (z: Boolean, s: String?) -> Unit){
-        TcpServer.INSTANCE.send(z,h,w,r,m,d,cc,st,t,c,u,p,dm,sm,s,callBack)
+    fun req(z: Boolean = false,h: Boolean = false,w:Boolean = false,r:Boolean = false,m:Boolean = false,d:Boolean = false,c:Boolean = false,st:Boolean = false,t:Int = 5,cs:String? = null,u:String? = null,p:Int,dm:Long = 100,sm:Long = 100,s: List<String>?,callBack: (z: Boolean, s: String?) -> Unit){
+        TcpServer.INSTANCE.send(z,h,w,r,m,d,c,st,t,cs,u,p,dm,sm,s,callBack)
     }
 }
